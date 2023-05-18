22153f0e2e9c5651f2b816aa7894b03190ce726c
unknown <engnegin200@gmail.com>
2023-05-18 18:16:27 +0330

    Practic1.js

---
+++ script.js
@@ -0,0 +1,17 @@
+ /*adad ha baraye a mitavan 1,2,0 bashad ta ham zero .odd,even ra khoroji bedeh*/
+
+let a=0;
+let b=6;
+        switch ( a ) {
+            case 0:
+                console.log("zero");
+                break;
+            default:
+                if ( a % 2 === 0) {
+                    console.log("even");
+                } else {
+                    console.log("odd");
+                }
+              }
+
+             
